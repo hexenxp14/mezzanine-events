21bf59d11d9945ba7ce036982296329f89988c86
David Loaiza <david@zooluciones.com>
2013-07-14 23:37:44 -0500

    Add i18n support

--- mezzanine_events/admin.py
+++ mezzanine_events/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from mezzanine.pages.admin import PageAdmin
 from .models import Event, EventContainer
 from copy import deepcopy
-
+from django.utils.translation import ugettext_lazy as _
 # event_admin_fieldsets = deepcopy(PageAdmin.fieldsets)
 # event_admin_fieldsets[0][1]["fields"]
 
 class EventAdmin (PageAdmin):
 	fieldsets = (
 		deepcopy(PageAdmin.fieldsets[0]),
-		("Event details",{
+		(_("Event details"),{
 			'fields': ('content', 'date', ('start_time', 'end_time'), 'location', 'mappable_location', ('lat', 'lon'), 'speakers', 'rsvp')
 		}),
 		deepcopy(PageAdmin.fieldsets[1]),
--- mezzanine_events/models.py
+++ mezzanine_events/models.py
@@ -8,20 +8,21 @@ from django.contrib.sites.models import Site
 from datetime import timedelta, datetime as dt
 from mezzanine.utils.sites import current_site_id
 from mezzanine.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 def _get_current_domain():
 	return Site.objects.get(id=current_site_id()).domain
 
 class Event(Page, RichText):
-	date = models.DateField()
-	start_time = models.TimeField()
-	end_time = models.TimeField()
-	speakers = models.TextField(blank=True, help_text="Leave blank if not relevant. Write one name per line.")
-	location = models.TextField()
-	mappable_location = models.CharField(max_length=128, blank=True, help_text="This address will be used to calculate latitude and longitude. Leave blank and set Latitude and Longitude to specify the location yourself, or leave all three blank to auto-fill from the Location field.")
-	lat = models.DecimalField(max_digits=10, decimal_places=7, blank=True, null=True, verbose_name="Latitude", help_text="Calculated automatically if mappable location is set.")
-	lon = models.DecimalField(max_digits=10, decimal_places=7, blank=True, null=True, verbose_name="Longitude", help_text="Calculated automatically if mappable location is set.")
-	rsvp = models.TextField(blank=True, help_text="RSVP information. Leave blank if not relevant. Emails will be converted into links.")
+	date = models.DateField(verbose_name=_("Date"))
+	start_time = models.TimeField(verbose_name=_("Start time"))
+	end_time = models.TimeField(verbose_name=_("End time"))
+	speakers = models.TextField(blank=True, verbose_name=_("Speakers"), help_text=_("Leave blank if not relevant. Write one name per line."))
+	location = models.TextField(verbose_name=_("Location"))
+	mappable_location = models.CharField(max_length=128, blank=True, verbose_name=_("Mappable location"), help_text=_("This address will be used to calculate latitude and longitude. Leave blank and set Latitude and Longitude to specify the location yourself, or leave all three blank to auto-fill from the Location field."))
+	lat = models.DecimalField(max_digits=10, decimal_places=7, blank=True, null=True, verbose_name=_("Latitude"), help_text=_("Calculated automatically if mappable location is set."))
+	lon = models.DecimalField(max_digits=10, decimal_places=7, blank=True, null=True, verbose_name=_("Longitude"), help_text=_("Calculated automatically if mappable location is set."))
+	rsvp = models.TextField(blank=True, verbose_name=_("RSVP"), help_text=_("RSVP information. Leave blank if not relevant. Emails will be converted into links."))
 
 	def speakers_list(self):
 		return [x for x in self.speakers.split("\n") if x.strip() != ""]
@@ -36,10 +37,10 @@ class Event(Page, RichText):
 		super(Event, self).clean()
 
 		if self.lat and not self.lon:
-			raise ValidationError("Longitude required if specifying latitude.")
+			raise ValidationError(_("Longitude required if specifying latitude."))
 
 		if self.lon and not self.lat:
-			raise ValidationError("Latitude required if specifying longitude.")
+			raise ValidationError(_("Latitude required if specifying longitude."))
 
 		if not (self.lat and self.lon) and not self.mappable_location:
 			self.mappable_location = self.location.replace("\n",", ")
@@ -49,9 +50,9 @@ class Event(Page, RichText):
 			try:
 				location, (lat, lon) = g.geocode(self.mappable_location.encode('utf-8'))
 			except GQueryError as e:
-				raise ValidationError("The mappable location you specified could not be found on {service}: \"{error}\" Try changing the mappable location, removing any business names, or leaving mappable location blank and using coordinates from getlatlon.com.".format(service="Google Maps", error=e.message))
+				raise ValidationError(_("The mappable location you specified could not be found on {service}: \"{error}\" Try changing the mappable location, removing any business names, or leaving mappable location blank and using coordinates from getlatlon.com.").format(service="Google Maps", error=e.message))
 			except ValueError as e:
-				raise ValidationError("The mappable location you specified could not be found on {service}: \"{error}\" Try changing the mappable location, removing any business names, or leaving mappable location blank and using coordinates from getlatlon.com.".format(service="Google Maps", error=e.message))
+				raise ValidationError(_("The mappable location you specified could not be found on {service}: \"{error}\" Try changing the mappable location, removing any business names, or leaving mappable location blank and using coordinates from getlatlon.com.").format(service="Google Maps", error=e.message))
 			self.mappable_location = location
 			self.lat = lat
 			self.lon = lon
@@ -73,12 +74,13 @@ class Event(Page, RichText):
 		super(Event, self).save(*args, **kwargs)
 	
 	class Meta:
-		verbose_name = "Event"
-
+		verbose_name = _("Event")
+                verbose_name_plural = _("Events")
+                
 class EventContainer (Page):
-	hide_children = models.BooleanField(default=True, verbose_name="Hide events in this container from navigation")
+	hide_children = models.BooleanField(default=True, verbose_name=_("Hide events in this container from navigation"))
 	class Meta:
-		verbose_name = "Event Container"
+		verbose_name = _("Event Container")
 	
 	def events(self):
 		"""Convenience method for getting at all events in a container, in the right order, from a template."""
